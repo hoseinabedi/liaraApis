eb636c1213d5c16ab78d418db731619fb13702a2
Hossein Abedi <hosein.abedi2000@gmail.com>
2023-09-13 19:35:22 +0330

    Structure completed

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({"message": "Welcome to the API"});
+});
+
+app.listen(3000 , () => console.log("App is running on http://localhost:3000"));
