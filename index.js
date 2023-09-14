4c4de1cebb3dc4152e69a7c68d342ce0ebecf590
Hossein Abedi <hosein.abedi2000@gmail.com>
2023-09-14 14:34:48 +0330

    Routes defined

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const router = require('./routes/liara');
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.status(200).json({"message": "Welcome to the API"});
 });
+
+app.use('/api/v1', router);
 
 app.listen(3000 , () => console.log("App is running on http://localhost:3000"));
